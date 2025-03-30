a052fbf766e82d602c4b9ae86071e126988a8a56
Thusathmi Sapumanage <169069405+ThusathmiSapumanage@users.noreply.github.com>
2025-03-30 16:17:24 +0530

    Add files via upload

---
+++ src/main/java/com/app/FoodMicroserviceDm2Application.java
@@ -0,0 +1,13 @@
+package com.example.FoodMicroservice_DM2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodMicroserviceDm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodMicroserviceDm2Application.class, args);
+	}
+
+}
